0cddba7fdc59cbf170cd3b0b305e35890601e0d3
BenGJ10 <bengj1015@gmail.com>
2026-08-15 13:10:03 +0530

    feat: add spring-audit implementation

--- src/main/java/com/bengj/hirers/HirersApplication.java
+++ src/main/java/com/bengj/hirers/HirersApplication.java
@@ -2,8 +2,10 @@ package com.bengj.hirers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImplementation")
 public class HirersApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/bengj/hirers/audit/AuditorAwareImplementation.java
@@ -0,0 +1,15 @@
+package com.bengj.hirers.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditorAwareImplementation")
+public class AuditorAwareImplementation implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("Anonymous");
+    }
+}

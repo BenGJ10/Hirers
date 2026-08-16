9336a53625c49e87212192a5e2a87a4c14762e57
BenGJ10 <bengj1015@gmail.com>
2026-08-16 12:26:41 +0530

    feat: add jwt utility and jwt token filter

---
+++ src/main/java/com/bengj/hirers/security/jwt/JwtTokenFilter.java
@@ -0,0 +1,82 @@
+package com.bengj.hirers.security.jwt;
+
+import com.bengj.hirers.constant.ApplicationConstants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
+
+    @Qualifier("publicPaths")
+    private final List<String> publicPaths;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain)
+                                    throws ServletException, IOException {
+        String authHeader = request.getHeader(ApplicationConstants.JWT_HEADER);
+        if(authHeader != null){
+            try {
+                // Extract the JWT token from the Authorization header
+                String jwt = authHeader.substring(7);
+
+                Environment env = getEnvironment();
+                String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY,
+                        ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
+
+                SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+                Claims claims = Jwts.parser()
+                        .verifyWith(secretKey)
+                        .build()
+                        .parseSignedClaims(jwt)
+                        .getPayload();
+
+                String username = String.valueOf(claims.get("username"));
+                String roles = String.valueOf(claims.get("roles"));
+
+                Authentication authentication = new UsernamePasswordAuthenticationToken(username, null,
+                        AuthorityUtils.commaSeparatedStringToAuthorityList(roles));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                System.out.println("Authentication successful");
+
+            } catch (ExpiredJwtException exception) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Token Expired");
+                return;
+            } catch (Exception e) {
+                throw new BadCredentialsException("Invalid token ");
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return publicPaths.stream().anyMatch(publicPath -> pathMatcher.match(publicPath, path));
+    }
+}
---
+++ src/main/java/com/bengj/hirers/security/jwt/JwtUtil.java
@@ -0,0 +1,58 @@
+package com.bengj.hirers.security.jwt;
+
+import com.bengj.hirers.constant.ApplicationConstants;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class JwtUtil {
+
+    private final Environment env;
+    private static final long EXPIRATION_TIME = 30L * 24 * 60 * 60 * 1000;
+
+    public String generateToken(Authentication authentication){
+        String token;
+
+        // Get the secret key from the environment variables or use the default value
+        String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY,
+                ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
+        
+                // Create a SecretKey object from the secret key string
+        SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+        // Fetch the user details from the authentication object
+        User fetchedUser = (User) authentication.getPrincipal();
+        if(fetchedUser == null){
+            return null;
+        }
+        
+        // Get the username and roles from the fetched user
+        String username = fetchedUser.getUsername();
+        String roles = fetchedUser.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(","));
+
+        // Build the JWT token using the Jwts builder
+        token = Jwts.builder()
+                .issuer("Hirers")
+                .subject("")
+                .claim("username", username)
+                .claim("roles", roles)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(secretKey)
+                .compact();
+
+        return token;
+    }
+}
